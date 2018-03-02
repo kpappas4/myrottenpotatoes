ac928bbb9870f7516f837c2ebe75293debbde837
Kostandino Pappas <kpappas4@uncc.edu>
2018-03-02 03:24:15 +0000

    Completed Sec 4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
 
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -16,7 +17,8 @@ class MoviesController < ApplicationController
 
 
   def create
-    @movie = Movie.create!(params[:movie])
+    #@movie = Movie.create!(params[:movie]) #old way
+    @movie = Movie.create!(movie_params)  # new way
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -28,7 +30,8 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
+    #@movie.update_attributes!(params[:movie])  # old way
+    @movie.update_attributes!(movie_params)  # new way  
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
@@ -40,4 +43,10 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  private
+
+    def movie_params
+      params.require(:movie).permit(:title, :rating, :description, :release_date)
+    end
+    
 end
